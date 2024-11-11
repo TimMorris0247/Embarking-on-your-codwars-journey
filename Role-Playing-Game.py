d87a819c3c347a00f9cd292cc59668cde5f3e6f6
TimMorris0247 <timmorris0247@gmail.com>
2024-11-11 13:34:42 -0500

    first Commit

---
+++ Role-Playing-Game.py
@@ -0,0 +1,4 @@
+print("""You've just woken up in a dark, scary forrest. Unsure of 
+how you got here, you try to find a clue that can help.""")
+
+
---
+++ codewars.py
@@ -0,0 +1,14 @@
+
+# Even Or Odd
+
+def even_or_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+
+# Convert a number to a string
+def number_to_string(num):
+    return str(num)
+
+
