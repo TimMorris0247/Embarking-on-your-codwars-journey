7ded526032151d1e23289aedfe1035c21bc81199
TimMorris0247 <timmorris0247@gmail.com>
2024-11-11 14:03:16 -0500

    Update codewars.py

--- codewars.py
+++ codewars.py
@@ -10,5 +10,13 @@ def even_or_odd(number):
 # Convert a number to a string
 def number_to_string(num):
     return str(num)
+    x = str(124)
+    print(x)
 
-
+# Vowel Count
+def get_count(sentence):
+    count = 0
+    for i in sentence:
+        if i in "aeiouAEIOU":
+            count = count + 1
+    return count
